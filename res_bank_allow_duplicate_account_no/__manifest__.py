9ad7e925ab67f132959d8056d77336ef70371e25
nibalKassim <nk@tmict.com>
2022-08-21 20:21:42 +0200

    Add files via upload

---
+++ res_bank_allow_duplicate_account_no/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Inspired from OCA Operating Units
+
+{
+    'name': "TMICT - Allow Duplicate Account No",
+    'summary': """
+        Allow Duplicate Account No.""",
+    'description': "",
+    'author': "TechnoMates",
+    'website': "http://www.tmict.com",
+    'category': 'TMICT',
+    'version': '15.0.1.0.0',
+    'depends': [
+        'base'
+    ],
+    'external_dependencies' : {},
+    'data': [
+    ],
+    'demo': [],
+    'application':False,
+    'installable': True,
+    'auto_install': False,
+    'uninstall_hook': '',
+    'pre_init_hook': '',
+    'post_init_hook': '',
+    'active':True,
+}
---
+++ res_bank_allow_duplicate_account_no/models/res_bank_allow_duplicate_account_no.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from collections.abc import Iterable
+
+from odoo import api, fields, models, _
+from odoo.osv import expression
+from odoo.exceptions import UserError
+
+import werkzeug.urls
+
+
+class ResPartnerBank(models.Model):
+    _name = 'res.partner.bank'
+
+    _sql_constraints = [
+        ('unique_number', 'unique(sanitized_acc_number, bank_id, company_id)', 'Account Number must be unique'),
+    ]
+
