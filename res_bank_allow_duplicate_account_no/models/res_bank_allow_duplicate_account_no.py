40668c1fa88625019ff41ab3e0a04da649c352c3
nibalKassim <nk@tmict.com>
2022-08-22 00:47:17 +0200

    Add files via upload

--- res_bank_allow_duplicate_account_no/models/res_bank_allow_duplicate_account_no.py
+++ res_bank_allow_duplicate_account_no/models/res_bank_allow_duplicate_account_no.py
@@ -14,7 +14,8 @@ import werkzeug.urls
 class ResPartnerBank(models.Model):
     _name = 'res.partner.bank'
 
-    _sql_constraints = [
-        ('unique_number', 'unique(sanitized_acc_number, bank_id, company_id)', 'Account Number must be unique'),
-    ]
-
+    def _auto_init(self, cr, context=None):
+        self._sql_constraints = [
+            ('unique_number', 'unique(sanitized_acc_number, bank_id, company_id)', 'Account Number must be unique')
+        ]
+          super(ResPartnerBank, self)._auto_init(cr, context)
